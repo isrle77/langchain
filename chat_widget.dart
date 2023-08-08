0e4ec314f4e06646c4cf9a077089c37b96a4c226
isrle77 <98024533+isrle77@users.noreply.github.com>
2023-08-08 09:06:28 +0300

    Add files via upload

---
+++ chat_widget.dart
@@ -0,0 +1,64 @@
+import 'package:aidoc/assets/constvar.dart';
+import 'package:aidoc/tools/service.dart';
+import 'package:aidoc/widgets/text_widget.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+class ChatWidget extends StatelessWidget {
+  const ChatWidget({super.key, required this.msg, required this.chatIndex});
+
+  final String msg;
+  final int chatIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Material(
+          color: chatIndex == 0 ? scafoldBGcolor : chatCard,
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  chatIndex == 0 ? ImgDir.userImg : ImgDir.botImg,
+                  height: 30,
+                  width: 30,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: chatIndex == 0
+                      ? TextWidget(
+                          label: msg,
+                        )
+                      : DefaultTextStyle(
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                          child: AnimatedTextKit(
+                            key: ValueKey(msg),
+                            isRepeatingAnimation: false,
+                            repeatForever: false,
+                            displayFullTextOnTap: true,
+                            totalRepeatCount: 1,
+                            animatedTexts: [
+                              TyperAnimatedText(
+                                msg.trim(),
+                              )
+                            ],
+                          ),
+                        ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
